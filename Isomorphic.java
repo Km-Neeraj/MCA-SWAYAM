2e5296a95a365afc305764fff0c0d299d5725ec7
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-05 11:42:51 +0530

    Add files via upload

---
+++ Isomorphic.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution 
+{
+   static boolean isIsomorphic(String s1,String s2)
+   {
+      int n=s1.length();
+      for(int i=0;i<n;i++)
+      {
+         char c1=s1.charAt(i);
+         char c2=s2.charAt(i);
+         for(int j=0;j<n;j++)
+         {
+              if(s1.charAt(j)==c1&&s2.charAt(j)!=c2)
+              return false;
+              if(s2.charAt(j)==c2&&s1.charAt(j)!=c1)
+              {
+                 return false;
+              }
+         }
+
+      }
+      return true;
+   }
+}
+public class Isomorphic {
+                         public static void main(String[] args) {
+                             
+                             String s1,s2;
+                             Scanner sc=new Scanner(System.in);
+                             s1=sc.next();
+                             s2=sc.next();
+                             if(Solution.isIsomorphic(s1,s2))
+                             {
+                                System.out.println("True");
+                             }
+                             else 
+                             {
+                                 System.out.println("False");
+                             }
+                             sc.close();
+                                                  
+                         }
+                         
+}
