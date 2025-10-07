8c5b18bd8cf23d041a42e223b65aa11936f7535f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-07 09:53:55 +0530

    Add files via upload

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,50 +1,41 @@
 import java.util.*;
-class Solution
- {
+class Solution 
+{
+   static int majority(int[]arr,int n)
+   {
+       Arrays.sort(arr);
+       int candidate=arr[n/2];
+       int c=0;
+       for(int num:arr)
+       {
+          if(num==candidate)
+          {
+            c++;
+          }
+       }
+       if(c>n/2)
+       return candidate;
+       else 
+       return 0;
+   }
+}
+public class MajorityElement {
+                         public static void main(String[] args) {
 
-                         int majorityElement(int[]arr,int n)
+                         int n;
+                         Scanner sc=new Scanner(System.in);
+                         n=sc.nextInt();
+                         System.out.println("Enter Array Elements");
+                         int[]arr=new int[n];
+                         
+                         for(int i=0;i<n;i++)
                          {
-                                                  int count=0;
-                              Arrays.sort(arr);
-                              int candidate=arr[n/2];
-                              for(int num:arr)
-                              {
-                                  if(num==candidate)
-                                  {
-                                                  count++;
-                                  }
-                              }
-                              if(count>n/2)
-                              {
-                                                  return candidate;
-                              }
-                              return -1;
+                             arr[i]=sc.nextInt();
+                         }
+                         int res=Solution.majority(arr,n);
+                         System.out.println("Majority element = "+res);
+                         sc.close();
+                                                  
                          }
                          
-}
-public class MajorityElement
-{
-public static void main(String[]abc)
-{
-    int n;
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the number of an array Elements");
-    n=sc.nextInt();
-    int []arr=new int[n];
-    System.out.println("Enter Array Elements");
-    for(int i=0;i<n;i++)
-    {
-          arr[i]=sc.nextInt();
-    }
-    Solution ob=new Solution();
-    int majority=ob.majorityElement(arr,n);
-    if(majority==-1)
-    {
-                         System.out.println("Majority Element doesn't exist");
-    }
-    else{
-    System.out.println("Majority Element = "+majority);
-    }
-    sc.close();
-}
 }
