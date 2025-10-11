4681f698ec2bc58d9396e32efe8495615babcc4a
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-11 22:17:24 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,29 @@
+class Person{
+      String name;
+      int age;
+      void show()
+      {
+                         System.out.print(name+" "+age);
+      }
+      Person()
+      {
+            System.out.println("Hello Person");
+            name="Ram";
+            age=10;
+
+      }
+      {
+                         System.out.println("OK");
+      }
+      {
+             System.out.println("Hello");
+      }
+}
+public class Main1 {
+   public static void main(String[]abc)
+   {
+      Person p1=new Person();
+      p1.show();
+   }
+                         
+}
