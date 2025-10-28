73a326b65a72994497fa089e30a964a1888d1321
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-28 12:05:50 +0530

    Add files via upload

---
+++ SearchInsertPosition.java
@@ -0,0 +1,31 @@
+
+
+class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (high >= low) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low;
+    }
+
+    public static void main(String[] args) {
+        SearchInsertPosition obj = new SearchInsertPosition();
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        int result = obj.searchInsert(nums, target);
+        System.out.println(result); // Output: 2
+    }
+}
+
+                         
+
