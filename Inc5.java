631ff238bd96468aa80a7bde607941670b48dd03
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-30 00:11:14 +0530

    Add files via upload

---
+++ Inc5.java
@@ -0,0 +1,12 @@
+public class Inc5 {
+                         public static void main(String[] args) {
+                         
+                           int a=15;
+                           int b=10;
+                           int res= a++ + --a + a*b++ +a--;
+                           System.out.println(res);
+                           System.out.println("Value of a= "+a+" Value of b = "+b);
+                           
+                         }
+                         
+}
