0cdffcd34391f5ea5e26c69593b2da2540d85557
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-30 00:17:31 +0530

    Add files via upload

---
+++ Inc6.java
@@ -0,0 +1,11 @@
+public class Inc6 {
+                         public static void main(String[] args) {
+                                                  
+                            int a=10;
+                            int b=20;
+                            int res= a*b+ ++b*10+a-- - --b*a++;
+                            System.out.println(res);
+
+                         }
+                         
+}
