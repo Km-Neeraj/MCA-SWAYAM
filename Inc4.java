7a76f210b57f37e62cbb1baa5fc5fc45af0a7ef1
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-29 23:59:39 +0530

    Add files via upload

---
+++ Inc4.java
@@ -0,0 +1,9 @@
+public class Inc4 {
+                         public static void main(String[] args) {
+                                                  
+                           int a=20;
+                           int res=a++ + --a +a*10;
+                           System.out.println(res);
+                         }
+                         
+}
