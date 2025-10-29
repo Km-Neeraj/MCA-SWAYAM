81df47814936eaff0864e2848d85361c0b28a9f2
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-29 23:53:59 +0530

    Add files via upload

---
+++ Inc1.java
@@ -0,0 +1,9 @@
+public class Inc1 {
+                         public static void main(String[] args) {
+                                                  
+                             int a=10;
+                             int res=a++ + --a - a++ + a--;
+                             System.out.println(res);
+                         }
+                         
+}
