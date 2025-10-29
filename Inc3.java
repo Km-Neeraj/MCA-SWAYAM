95cf214b7180dfa35bc67fb3b752c963ba5cdca1
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-29 23:58:42 +0530

    Add files via upload

---
+++ Inc3.java
@@ -0,0 +1,9 @@
+public class Inc3 {
+                         public static void main(String[] args) {
+                                                  
+                            int a=10,b=20;
+                            int res=a+b + a++ + --b-a--;
+                            System.out.println(res);
+                         }
+                         
+}
