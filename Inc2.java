e4d96b0d4b83eee7596ec96e7eed00de8866137f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-10-29 23:56:25 +0530

    Add files via upload

---
+++ Inc2.java
@@ -0,0 +1,9 @@
+public class Inc2 {
+                         public static void main(String[] args) {
+                                                  
+                            int a=20;
+                            int res= a++ + a*10 + a-- - --a;
+                            System.out.println(res);
+                         }
+                         
+}
