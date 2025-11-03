ef2662a428f6d46189e58829f90e6545beea520e
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-03 23:59:52 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,28 +1,20 @@
-import java.util.*;
 public class Palindrome {
-                         public static void main(String[]abc)
-                         {
-                             int lowerLimit,upperLimit;
-                             Scanner sc=new Scanner(System.in);
-                             lowerLimit=sc.nextInt();
-                             upperLimit=sc.nextInt();
-                             for(int i=lowerLimit;i<=upperLimit;i++)
-                             {
-                                int temp=i;
-                                int rev=0,rem;
-                                int num=i;
-                                while(num!=0)
-                                {
-                                    rem=num%10;
-                                    rev=rev*10+rem;
-                                    num=num/10;
-                                }
-                                if(rev==temp)
-                                {
-                                   System.out.print(i+" ");
-                                }
-                             }
-                             sc.close();
+                         public static void main(String[] args) {
+                           int num=121;
+                           int temp=num;
+                           int rev=0;
+                           while(num!=0)
+                           {
+                              int rem=num%10;
+                              rev=rev*10+rem;
+                              num=num/10;
+                           }
+                           if(temp==rev)
+                           System.out.println("Palindrome");
+                           else 
+                           {
+                             System.out.println("Not Palindrome");
+                           }
 
                          }
                          
