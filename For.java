fdb23c1204825ce1cb0d53605bf594d634d3640d
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-03 23:57:03 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,18 @@
+public class For {
+                         public static void main(String[]abc)
+                         {
+                             for(int i=0;i<=5;i++)
+                             {
+                               System.out.print("Hello "+" ");
+                               for(int j=i;j<=5;j++)
+                               {
+                                  if(j==3)
+                                  break;
+                             System.out.print("Welcome"+" ");
+                               }
+                               System.out.println();
+                               System.out.println();
+                             }
+                         }
+                         
+}
