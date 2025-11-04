bceb4f65db3959ad1f4191687b12d7686c845f4f
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-05 00:36:47 +0530

    Add files via upload

---
+++ CountSubArray.java
@@ -0,0 +1,45 @@
+//Count Subarrays with a Given Sum 
+import java.util.*;
+class Solution 
+{
+   static int maxLength(int[]arr,int n,int k)
+   {
+      int res=0;
+      for(int i=0;i<n;i++)
+      {
+          int sum=0;
+          for(int j=i;j<n;j++)
+          {
+              sum=sum+arr[j];
+             if(sum==k)
+             {
+                 res++;
+             }
+          }
+      }
+      return res;
+   }
+}
+public class CountSubArray {
+                         public static void main(String[] args) {
+                                                  
+                            int n;
+                            Scanner sc=new Scanner(System.in);
+                            n=sc.nextInt();
+                            int[]arr=new int[n];
+                            for(int i=0;i<n;i++)
+                            {
+                               arr[i]=sc.nextInt();
+                            }
+                            System.out.println("Enter value of sum");
+                            int k=sc.nextInt();
+
+                            int len=Solution.maxLength(arr,n,k);
+                            System.out.println("Maximum length of subarray with sum = "+len);
+                            sc.close();
+                         }
+                         
+}
+
+                         
+
