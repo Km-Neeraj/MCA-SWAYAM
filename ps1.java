e892170ad448a4ed897a991cd6e7b512a0ce4e74
Neeraj Yadav <neerajydv1113@gmail.com>
2025-11-17 22:48:48 +0530

    Add files via upload

---
+++ ps1.java
@@ -0,0 +1,11 @@
+public class ps1 {
+                         public static void main(String[] args) {
+                                                  
+                            int a=5;
+                            int b=3;
+                            int c=a^b<<1&7;
+                            int d=~c|a;
+                            System.out.println(d);
+                            
+                         }
+}
