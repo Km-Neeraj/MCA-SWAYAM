1dab8c629aea7f6df431ed13cfd3aa91973ebfdf
Neeraj Yadav <neerajydv1113@gmail.com>
2025-12-10 21:43:37 +0530

    Add files via upload

--- Difference.java
+++ Difference.java
@@ -1,31 +1,30 @@
+import java.util.*;
+class Solution 
+{
+   static char result(String s,String t)
+   {
+      char res=0;
+      for(int i=0;i<s.length();i++)
+      {
+                         res ^=s.charAt(i);
+      }
+      for(int i=0;i<t.length();i++)
+      {
+         res^=t.charAt(i);
+      }
+      return res;
 
-
-class Solution {
-    public char findTheDifference(String s, String t) {
-        int[] arr = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            arr[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-            arr[t.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (arr[i] != 0)
-                return (char) (i + 'a');
-        }
-        return ' ';
-    }
+   }
 }
-
 public class Difference {
-    public static void main(String[] args) {
-        String s = "abcd";
-        String t = "abcde";
-        Solution obj = new Solution();
-        char result = obj.findTheDifference(s, t);
-        System.out.println("The extra character is: " + result);
-    }
-}
-
+                         public static void main(String[] args) {
+                                                  
+                         Scanner sc=new Scanner(System.in);
+                         String s=sc.next();
+                         String t=sc.next();
+            char res=Solution.result(s,t);
+            System.out.println("Difference = "+res);
+            sc.close();
+                         }
                          
-
+}
