d706af15742b9a2cb8537c9d9d48a831cdf11a83
Neeraj Yadav <neerajydv1113@gmail.com>
2026-02-04 11:12:54 +0530

    Add files via upload

--- Union1.java
+++ Union1.java
@@ -2,14 +2,54 @@
 import java.util.*;
 class Solution 
 {
-   static LinkedHashSet<Integer> union(int[]arr1,int n,int arr2[],int m)
+
+  static boolean binarySearch(int[]arr1,int key)
+  {
+     int low=0,high=arr1.length-1;
+
+     while(low<=high)
+     {
+        int mid=low+(high-low)/2;
+
+        if(arr1[mid]==key)
+                         return true;
+         else if(arr1[mid]<key)
+               low=mid+1;
+          else 
+                         high=mid-1;
+     }
+     return false;
+  }
+
+
+
+
+   static void union(int[]arr1,int n,int arr2[],int m)
    {
-       LinkedHashSet<Integer> res=new LinkedHashSet<>();
+       Arrays.sort(arr1);
+       int lastPrinted=Integer.MIN_VALUE;  // For tracking duplicate Values
        for(int num:arr1)
-            res.add(num);
-        for(int num: arr2)
-              res.add(num);
-       return res;
+       {
+            if(num!=lastPrinted)
+                         {
+                                   System.out.print(num+" ");
+                                   lastPrinted=num;
+                         }
+            
+           
+       }
+        
+        for(int i=0;i<m;i++)
+        {
+             if(!binarySearch(arr1,arr2[i])&&arr2[i]!=lastPrinted)
+             {
+                         System.out.print(arr2[i]+" ");
+                         lastPrinted=arr2[i];
+             }
+        }
+                         
+
+
    }
 }
 public class Union1 {
@@ -35,12 +75,42 @@ public class Union1 {
                             {
                               arr2[i]=sc.nextInt();
                             }
-                          LinkedHashSet<Integer> res=Solution.union(arr1,n,arr2, m);
-                          for(int num:res)
-                          {
-                                                  System.out.print(num+" ");
-                          }
+                          Solution.union(arr1,n,arr2, m);
+                          
                           sc.close();
                          }
                          
 }
+
+
+/* Example:
+    arr1[]={2,2,3,3,4};
+    arr2[]={4, 5,5,5,5};
+    */
+
+    /* Time: O(n log n + m log n)
+
+Space: O(1) (no extra data structure)  
+
+O(n log n)   // sorting
++ O(n)      // printing arr1
++ O(m log n)// searching arr2
+
+Drop smaller terms (DSA rule)
+
+O(n) is smaller than O(n log n)
+
+Final Time Complexity:
+
+O(n log n + m log n)
+
+
+
+
+
+
+
+
+
+
+*/
