01db420654dbdd19f8f6cb560edaa88b3a2c82cb
Neeraj Yadav <neerajydv1113@gmail.com>
2026-02-07 10:20:58 +0530

    Add files via upload

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,40 +1,30 @@
-//    Find the Number that Appears Once, and Other Numbers Appear Twice 
 import java.util.*;
-class Solution 
+class Solution
 {
    static int singleNumber(int[]arr,int n)
    {
-        for(int i=0;i<n;i++)
-        {
-                         int count=0;
-            for(int j=0;j<n;j++)
-            {
-                if(arr[i]==arr[j]) 
-                count++;
-            }
-            if(count==1)
-            return arr[i];
-        }
-        return 0;
-   }
+       int xr=0;
+       for(int num:arr)
+       {
+        xr=xr^num;
+       }
+       return xr;
+   }                      
 }
+
 public class SingleNumber {
-                         public static void main(String[]abc)
-                         {
-                            int n;
-                            Scanner sc=new Scanner(System.in);
-                            System.out.println("Enter the size of an array");
-                            n=sc.nextInt();
-                            int[]arr=new int[n];
-                            for(int i=0;i<n;i++)
-                            {
-                              arr[i]=sc.nextInt();
-                            }
-                            int single=Solution.singleNumber(arr,n);
-                            System.out.println("Unique number = "+single);
-                            sc.close();
-
-
+                         public static void main(String[] args) {
+                                                  
+                                                  Scanner sc=new Scanner(System.in);
+                                                  int n=sc.nextInt();
+                                                  int[]arr=new int[n];
+                                                  for(int i=0;i<n;i++)
+                                                  {
+                                                     arr[i]=sc.nextInt();
+                                                  }
+                                     int num=Solution.singleNumber(arr,n);
+                                     System.out.println("Single number = "+num);
+                                     sc.close();
                          }
                          
 }
